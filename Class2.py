36ab61e504a9d89d912b7781c196a419faf9db0c
hooria-tariq <whoriyahtk@gmail.com>
2025-07-03 19:31:15 +0500

    Class 2

---
+++ Class2.py
@@ -0,0 +1,77 @@
+# marks=float(input("Enter Marks of Student: "))
+# if marks>=90 and marks<=100:
+#     print("the grade is A")
+# elif marks>=80 and marks<=90:
+#     print("the grade is B")
+# elif marks>=70 and marks<=80:
+#     print("the grade is C")
+# elif marks>=60 and marks<=70:
+#     print("the grade is D")
+# elif marks>=50 and marks<=60:
+#     print("the grade is E")
+# elif marks<=40 and marks>=0:
+#     print("the grade is F")
+# else:
+#     print("invalid input")
+
+# range(6)
+# 0 to 5
+#range(1,3)prints 1 and 2 and 1 here is starting point if not mentioned is zero by default
+# for i in range(1,11): #[0,1,2]
+#     print(i)
+
+# for i in "Hooria":
+#     print(i)
+
+# fruit_list= ["cherry","orange","mango","cherry"]
+# #index 0,1,2,3
+# #print(fruit_list[1])
+# for fruit in fruit_list:
+#     print(fruit)
+# print(len(fruit_list))    
+
+# fruit_list= ["cherry","orange","mango","cherry"]
+# roll_no=[1,2,3,4]
+# list_bool=[True,False,True]
+# list_mul=[1,"scince", True]
+# print(fruit_list)
+# print(roll_no)
+# print(list_bool)
+# print(list_mul)
+# print(type(fruit_list))
+
+# list1=list([1,2,3])
+# list2=[]
+# print(list1)
+
+#even or odd
+# num=int(input('Enter Num: '))
+# if num%2==0:
+#     print("Number is even")
+# else:
+#     print("Number is odd")
+
+# for num in range(1,101)
+# if num % 2 != 0:
+#     print(num)
+
+#table user 
+# table_num= int(input("Enter nnum: "))
+#  for i in range(1,11):
+#   print(f"{table_num}x{i}={table_num*i}")
+
+# num  =[1, 2, 3, 4, 5, 6, 7]
+#  for x in num:
+#     print(x)
+#     if x==4:
+#      break
+
+for i in range(5):
+   pass
+num= [1, 2, 3, 4, 5, 6, 7]
+for x in num:
+    if x == 3:
+     #continue
+     break
+print(x)
+
